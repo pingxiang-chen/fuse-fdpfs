238957e70862a053f09fce98574c657a94de3ce6
Ping-Xiang Chen <p.x.chen@uci.edu>
2024-07-25 11:55:45 -0700

    modified fdpfs.h

--- fdpfs.h
+++ fdpfs.h
@@ -3,9 +3,9 @@
 
 struct fdpfs_dev {
 	/* Device file path and basename */
-    char	*path;
-    char    *name;
-	
+	char	*path;
+	char	*name;
+
 	/* Flags and features */
 	unsigned int flags;
 	unsigned long long features;
@@ -20,18 +20,17 @@ struct fdpfs_dev {
 	unsigned int nnss;
 	unsigned int runs;
 	unsigned int erutl;
-	struct nvme_fdp_ruh_desc *ruhs;
-	
+	struct	nvme_fdp_ruh_desc *ruhs;
+
 	/* Device file descriptor */
-	int          fd;
+	int	fd;
 
 	/* Device namespace id */
-	int nsid;
+	int	nsid;
 };
 
 /* fdpfs related functions */
 void fdpfs_close_dev(struct fdpfs_dev *dev);
 int fdpfs_open_dev(struct fdpfs_dev *dev, bool check_overwrite);
-
 
 #endif /* FDPFS_H */
