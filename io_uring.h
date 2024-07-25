2c122284d8f27ebe9a743a594599654154e82dac
Ping-Xiang Chen <p.x.chen@uci.edu>
2024-07-25 11:53:03 -0700

    modified io_uring.h

--- io_uring.h
+++ io_uring.h
@@ -21,78 +21,78 @@
 #define NVME_IDENTIFY_CSI_SHIFT 24
 
 enum fdpfs_ddir {
-    DDIR_READ = 0,
-    DDIR_WRITE = 1,
-    DDIR_TRIM = 2,
-    DDIR_SYNC = 3,
-    DDIR_DATASYNC,
-    DDIR_SYNC_FILE_RANGE,
-    DDIR_WAIT,
-    DDIR_LAST,
-    DDIR_INVAL = -1,
-    DDIR_TIMEOUT = -2,
+	DDIR_READ = 0,
+	DDIR_WRITE = 1,
+	DDIR_TRIM = 2,
+	DDIR_SYNC = 3,
+	DDIR_DATASYNC,
+	DDIR_SYNC_FILE_RANGE,
+	DDIR_WAIT,
+	DDIR_LAST,
+	DDIR_INVAL = -1,
+	DDIR_TIMEOUT = -2,
 
-    DDIR_RWDIR_CNT = 3,
-    DDIR_RWDIR_SYNC_CNT = 4,
+	DDIR_RWDIR_CNT = 3,
+	DDIR_RWDIR_SYNC_CNT = 4,
 };  
 
 struct io_sq_ring {
-    unsigned *head;
-    unsigned *tail;
-    unsigned *ring_mask;
-    unsigned *ring_entries;
-    unsigned *flags;
-    unsigned *array;
+	unsigned *head;
+	unsigned *tail;
+	unsigned *ring_mask;
+	unsigned *ring_entries;
+	unsigned *flags;
+	unsigned *array;
 };
 
 struct io_cq_ring {
-    unsigned *head;
-    unsigned *tail;
-    unsigned *ring_mask;
-    unsigned *ring_entries;
-    struct io_uring_cqe *cqes;
+	unsigned *head;
+	unsigned *tail;
+	unsigned *ring_mask;
+	unsigned *ring_entries;
+	struct io_uring_cqe *cqes;
 };
 
 struct ioring_data {
-    int ring_fd;
+	int ring_fd;
 
-    /* struct io_u **io_u_index; */
-    /* char *md_buf; */
+	/* struct io_u **io_u_index; */
+	/* char *md_buf; */
 
-    /* int *fds; */
+	/* int *fds; */
 
-    struct io_sq_ring sq_ring;
-    struct io_uring_sqe *sqes;
-    /* struct iovec *iovecs; */
-    /* unsigned sq_ring_mask; */
+	struct io_sq_ring sq_ring;
+	struct io_uring_sqe *sqes;
+	/* struct iovec *iovecs; */
+	/* unsigned sq_ring_mask; */
 
-    struct io_cq_ring cq_ring;
-    enum fdpfs_ddir ddir;
-	
+	struct io_cq_ring cq_ring;
+	enum fdpfs_ddir ddir;
+
 	uint32_t dtype;
 	uint32_t dspec;
-	
+
 	/* Device namespace id */
 	int nsid;
-	
+
 	unsigned index;
 
-	
+
 	char *orig_buffer;
 	size_t orig_buffer_size;
-	
+
 	/* unsigned cq_ring_mask; */
 
-    /* int queued; */
-    /* int cq_ring_off; */
-    /* unsigned iodepth; */
-    /* int prepped; */
+	/* int queued; */
+	/* int cq_ring_off; */
+	/* unsigned iodepth; */
+	/* int prepped; */
 
-    /* struct ioring_mmap mmap[3]; */
+	/* struct ioring_mmap mmap[3]; */
 
-    /* struct cmdprio cmdprio; */
+	/* struct cmdprio cmdprio; */
 
-    /* struct nvme_dsm_range *dsm; */
+	/* struct nvme_dsm_range *dsm; */
 };
 
 void fdpfs_ioring_probe(int ring_fd);
