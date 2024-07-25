530d8b46820de19914ab4ecf3f6bc6e6a818445c
Ping-Xiang Chen <p.x.chen@uci.edu>
2024-07-25 11:48:43 -0700

    modified io_uring.c

--- io_uring.c
+++ io_uring.c
@@ -10,16 +10,16 @@ int fdpfs_ioring_queue_init(struct ioring_data* ld){
 	p.flags |= IORING_SETUP_SQE128;
 	p.flags |= IORING_SETUP_CQE32;
 
-	/*   
-	*	Clamp CQ ring size at our SQ ring size, we don't need more entries 
-	*	than that.
+	/*
+	 * Clamp CQ ring size at our SQ ring size, 
+	 * we don't need more entries than that.
 	*/
 	p.flags |= IORING_SETUP_CQSIZE;
 	p.cq_entries = depth;
 
-	/*   
-	 *   Setup COOP_TASKRUN as we don't need to get IPI interrupted for
-	 *   completing IO operations.
+	/*
+	 * Setup COOP_TASKRUN as we don't need to get IPI interrupted for
+	 * completing IO operations.
 	*/
 	p.flags |= IORING_SETUP_COOP_TASKRUN;
 	
@@ -52,7 +52,7 @@ void fdpfs_ioring_probe(int ring_fd){
 	p = calloc(1, sizeof(*p) + 256 * sizeof(struct io_uring_probe_op));
 	
 	if (!p)
-        return;
+		return;
     
 	ret = syscall(__NR_io_uring_register, ring_fd,
             IORING_REGISTER_PROBE, p, 256);
@@ -67,27 +67,31 @@ out:
 
 int fdpfs_ioring_mmap(struct ioring_data *ld, struct io_uring_params *p)
 {   
-    struct io_sq_ring *sring = &ld->sq_ring;
-    struct io_cq_ring *cring = &ld->cq_ring;
-    void *sq_ptr, *cq_ptr;
-    
-    int sring_sz = p->sq_off.array + p->sq_entries * sizeof(__u32);
-   	int cring_sz = p->cq_off.cqes + p->cq_entries * sizeof(struct io_uring_cqe);
+	struct io_sq_ring *sring = &ld->sq_ring;
+	struct io_cq_ring *cring = &ld->cq_ring;
+	void *sq_ptr, *cq_ptr;
 
-	/* Map in the submission and completion queue ring buffers.
-     * Older kernels only map in the submission queue, though.
-     * */
-    sq_ptr = mmap(0, sring_sz, PROT_READ | PROT_WRITE, 
-            MAP_SHARED | MAP_POPULATE,
-            ld->ring_fd, IORING_OFF_SQ_RING);
-    
+	int sring_sz = p->sq_off.array + p->sq_entries * sizeof(__u32);
+	int cring_sz = p->cq_off.cqes + p->cq_entries * sizeof(struct io_uring_cqe);
+
+	/*
+	 * Map in the submission and completion queue ring buffers.
+	 * Older kernels only map in the submission queue, though.
+	*/
+
+	sq_ptr = mmap(0, sring_sz, PROT_READ | PROT_WRITE, 
+			MAP_SHARED | MAP_POPULATE, 
+			ld->ring_fd, IORING_OFF_SQ_RING);
+	
 	if (sq_ptr == MAP_FAILED) {
-        perror("mmap failed");
-        return 1;
-    }	
+		perror("mmap failed");
+		return 1;
+	}	
 
-	/* Save useful fields in a global app_io_sq_ring struct for later
-     * easy reference */
+	/*
+	 * Save useful fields in a global app_io_sq_ring struct for later 
+	 * easy reference 
+	*/
     sring->head = sq_ptr + p->sq_off.head;
     sring->tail = sq_ptr + p->sq_off.tail;
     sring->ring_mask = sq_ptr + p->sq_off.ring_mask;
@@ -108,14 +112,16 @@ int fdpfs_ioring_mmap(struct ioring_data *ld, struct io_uring_params *p)
 	cq_ptr = mmap(0, cring_sz, PROT_READ | PROT_WRITE,
 			MAP_SHARED | MAP_POPULATE, ld->ring_fd, IORING_OFF_CQ_RING);
 
-	/* Save useful fields in a global app_io_cq_ring struct for later
-    * easy reference */
-    cring->head = cq_ptr + p->cq_off.head;
-    cring->tail = cq_ptr + p->cq_off.tail;
-    cring->ring_mask = cq_ptr + p->cq_off.ring_mask;
-    cring->ring_entries = cq_ptr + p->cq_off.ring_entries;
-    cring->cqes = cq_ptr + p->cq_off.cqes;
-	
+	/*
+	 * Save useful fields in a global app_io_cq_ring struct for later
+	 * easy reference 
+	*/
+	cring->head = cq_ptr + p->cq_off.head;
+	cring->tail = cq_ptr + p->cq_off.tail;
+	cring->ring_mask = cq_ptr + p->cq_off.ring_mask;
+	cring->ring_entries = cq_ptr + p->cq_off.ring_entries;
+	cring->cqes = cq_ptr + p->cq_off.cqes;
+
 	return 0;
 }
 
@@ -123,6 +129,6 @@ int fdpfs_ioring_mmap(struct ioring_data *ld, struct io_uring_params *p)
 int io_uring_enter(int ring_fd, unsigned int to_submit,
                           unsigned int min_complete, unsigned int flags)
 {
-    return (int)syscall(__NR_io_uring_enter, ring_fd, to_submit, min_complete,
-                   flags, NULL, 0);
+	return (int)syscall(__NR_io_uring_enter, ring_fd, to_submit, 
+			min_complete, flags, NULL, 0);
 }
